e4251a8a4efd8fd34a15792a1d031d197b3d8799
Libing Chen <libing.chen@gmail.com>
2024-08-31 11:57:32 +0800

    chore: add build_jbang_app with Clap

--- src/jbang.rs
+++ src/jbang.rs
@@ -1,3 +1,128 @@
+//! clap App for command cli
+use clap::{Command, Arg, ArgAction};
+
+pub const VERSION: &str = "0.1.0";
+
 fn main() {
-    println!("Hello, jbang!");
+    let app = build_jbang_app();
+    let _matches = app.get_matches();
+}
+
+pub fn build_jbang_app() -> Command {
+    let run_command = Command::new("run")
+        .about("Builds and runs provided script.")
+        .arg(
+            Arg::new("main")
+                .short('m')
+                .long("main")
+                .help("Main class to use when running. Used primarily for running jar's.")
+                .required(true)
+        )
+        .arg(
+            Arg::new("scriptOrFile")
+                .help("A reference to a source file")
+                .index(1)
+                .required(false)
+        )
+        .arg(
+            Arg::new("userParams")
+                .help("Parameters to pass on to the script.")
+                .required(false)
+                .index(2)
+                .num_args(1..)
+        );
+    let build_command = Command::new("build")
+        .about("Compiles and stores script in the cache.")
+        .arg(
+            Arg::new("build-dir")
+                .long("build-dir")
+                .num_args(1)
+                .help("Use given directory for build results")
+                .required(false)
+        )
+        .arg(
+            Arg::new("scriptOrFile")
+                .help("A reference to a source file")
+                .index(1)
+                .required(false)
+        );
+    let init_command = Command::new("init")
+        .about("Builds and runs provided script.")
+        .arg(
+            Arg::new("template")
+                .short('t')
+                .long("template")
+                .num_args(1)
+                .help("Init script with a java class useful for scripting")
+                .required(true)
+        )
+        .arg(
+            Arg::new("scriptOrFile")
+                .help(" file or URL to a Java code file")
+                .index(1)
+                .required(false)
+        )
+        .arg(
+            Arg::new("params")
+                .help("Parameters to pass on to the generation.")
+                .required(false)
+                .index(2)
+                .num_args(1..)
+        );
+    Command::new("jbang")
+        .version(VERSION)
+        .about("jbang - Unleash the power of Java")
+        .arg(
+            Arg::new("config")
+                .long("config")
+                .num_args(1)
+                .help("Path to config file to be used instead of the default")
+                .required(false),
+        )
+        .arg(
+            Arg::new("fresh")
+                .long("fresh")
+                .action(ArgAction::SetTrue)
+                .help("Make sure we use fresh (i.e. non-cached) resources.")
+                .required(false),
+        )
+        .arg(
+            Arg::new("insecure")
+                .long("insecure")
+                .action(ArgAction::SetTrue)
+                .help("Enable insecure trust of all SSL certificates.")
+                .required(false),
+        )
+        .arg(
+            Arg::new("offline")
+                .short('o')
+                .long("offline")
+                .action(ArgAction::SetTrue)
+                .help("Work offline. Fail-fast if dependencies are missing.")
+                .required(false),
+        )
+        .arg(
+            Arg::new("preview")
+                .long("preview")
+                .action(ArgAction::SetTrue)
+                .help("Enable jbang preview features.")
+                .required(false),
+        )
+        .arg(
+            Arg::new("quiet")
+                .long("quiet")
+                .action(ArgAction::SetTrue)
+                .help("jbang will be quiet, only print when error occurs.")
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .long("verbose")
+                .action(ArgAction::SetTrue)
+                .help("jbang will be verbose on what it does.")
+                .required(false),
+        )
+        .subcommand(run_command)
+        .subcommand(build_command)
+        .subcommand(init_command)
 }
