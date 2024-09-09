5809912153f20ce58a571b86b5ebdb58c2750e5e
Libing Chen <libing.chen@gmail.com>
2024-09-09 09:29:43 +0800

    test: add unit tests

--- src/common.rs
+++ src/common.rs
@@ -13,3 +13,28 @@ fn unpack_tgz(tgz_file: &str, target_dir: &str) {
     let mut archive = Archive::new(tar);
     archive.unpack(target_dir).unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use super::*;
+
+    #[test]
+    fn test_download() {
+        let download_dir = dirs::download_dir().unwrap();
+        let target_file_path = download_dir.join("apache-maven-3.9.9-bin.tar.gz");
+        let http_url = "https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz";
+        http_download(http_url, target_file_path.to_str().unwrap());
+        assert!(target_file_path.exists());
+    }
+
+    #[test]
+    fn test_unpack_tgz() {
+        let dir = env::temp_dir();
+        let download_dir = dirs::download_dir().unwrap();
+        let target_file_path = download_dir.join("apache-maven-3.9.9-bin.tar.gz");
+        let dest_dir = dir.join("maven-20240910");
+        unpack_tgz(target_file_path.to_str().unwrap(), dest_dir.to_str().unwrap());
+        println!("{}",dest_dir.to_str().unwrap());
+    }
+}
