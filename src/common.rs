10a4936d4276aca85821d10c502f34d2f3114ada
Libing Chen <libing.chen@gmail.com>
2024-09-10 21:45:01 +0800

    chore: polishing

--- src/common.rs
+++ src/common.rs
@@ -1,20 +1,10 @@
 use std::collections::HashMap;
-use std::fs::File;
 use std::io;
 use std::process::{Command, Output, Stdio};
-use flate2::read::GzDecoder;
-use tar::Archive;
 
-fn http_download(http_url: &str, target_file_path: &str) {
+pub fn http_download(http_url: &str, target_file_path: &str) {
     let http_url = http_url;
     oneio::download(http_url, target_file_path, None).unwrap();
-}
-
-fn unpack_tgz(tgz_file: &str, target_dir: &str) {
-    let tar_gz = File::open(tgz_file).unwrap();
-    let tar = GzDecoder::new(tar_gz);
-    let mut archive = Archive::new(tar);
-    archive.unpack(target_dir).unwrap();
 }
 
 pub fn run_command(command_name: &str, args: &[&str]) -> io::Result<Output> {
@@ -34,7 +24,7 @@ pub fn run_command_with_env_vars(command_name: &str, args: &[&str], working_dir:
             command.env(key, value);
         }
     }
-     command
+    command
         .envs(std::env::vars())
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
@@ -44,7 +34,6 @@ pub fn run_command_with_env_vars(command_name: &str, args: &[&str], working_dir:
 
 #[cfg(test)]
 mod tests {
-    use std::env;
     use super::*;
 
     #[test]
@@ -54,15 +43,5 @@ mod tests {
         let http_url = "https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz";
         http_download(http_url, target_file_path.to_str().unwrap());
         assert!(target_file_path.exists());
-    }
-
-    #[test]
-    fn test_unpack_tgz() {
-        let dir = env::temp_dir();
-        let download_dir = dirs::download_dir().unwrap();
-        let target_file_path = download_dir.join("apache-maven-3.9.9-bin.tar.gz");
-        let dest_dir = dir.join("maven-20240910");
-        unpack_tgz(target_file_path.to_str().unwrap(), dest_dir.to_str().unwrap());
-        println!("{}", dest_dir.to_str().unwrap());
     }
 }
