acf655336d7051722f64158d26f038ca90718930
Libing Chen <libing.chen@gmail.com>
2024-09-18 15:05:23 +0800

    app: sdkman_cli mod

---
+++ src/sdkman_cli/mod.rs
@@ -0,0 +1,29 @@
+const SDKMAN_CANDIDATES_API: &str = "https://api.sdkman.io/2";
+pub fn get_sdkman_platform() -> String {
+    let os_name = match std::env::consts::OS {
+        "macos" => "darwin",
+        "windows" => "windows",
+        "linux" => "linux",
+        &_ => ""
+    };
+    let arch_name = match std::env::consts::ARCH {
+        "aarch64" => "arm64",
+        "x86_64" => "x64",
+        &_ => "exotic"
+    };
+    if arch_name == "exotic" {
+        "exotic".to_owned()
+    } else {
+        format!("{}{}", os_name, arch_name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sdkman_platform() {
+        println!("{}", get_sdkman_platform());
+    }
+}
