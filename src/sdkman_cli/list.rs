f5bbad01a138a5bdedf491a414d6eca1cbdc96c2
Libing Chen <libing.chen@gmail.com>
2024-09-18 20:25:45 +0800

    chore: add current version and installed versions for list_candidate

--- src/sdkman_cli/list.rs
+++ src/sdkman_cli/list.rs
@@ -1,5 +1,5 @@
 use crate::common::http_text;
-use crate::sdkman_cli::{get_sdkman_platform, SDKMAN_CANDIDATES_API};
+use crate::sdkman_cli::{get_installed_candidate_default_version, get_sdkman_platform, sdkman_home, SDKMAN_CANDIDATES_API};
 
 pub fn manage_list(list_matches: &clap::ArgMatches) {
     if let Some(candidate_name) = list_matches.get_one::<String>("candidate") {
@@ -13,11 +13,41 @@ pub fn list_all_candidates() {
     let list_url = format!("{}/candidates/list", SDKMAN_CANDIDATES_API);
     println!("{}", http_text(&list_url));
 }
+
 pub fn list_candidate(candidate_name: &str) {
-    let list_url = format!("{}/candidates/{}/{}/versions/list?installed=",
+    let current_version = get_installed_candidate_default_version(candidate_name);
+    let mut installed_versions: Vec<String> = vec![];
+    let candidate_repo = sdkman_home().join("candidates").join(candidate_name);
+    // read sub directories for candidate_repo
+    if candidate_repo.exists() {
+        let entries = std::fs::read_dir(candidate_repo).unwrap();
+        for entry in entries {
+            let entry = entry.unwrap();
+            if entry.path().is_dir() {
+                let version = entry.file_name().into_string().unwrap();
+                if version != "current" {
+                    installed_versions.push(version);
+                }
+            }
+        }
+    }
+    let list_url = format!("{}/candidates/{}/{}/versions/list?current={}&installed={}",
                            SDKMAN_CANDIDATES_API,
                            candidate_name,
-                           get_sdkman_platform()
+                           get_sdkman_platform(),
+                           current_version,
+                           installed_versions.join(",")
     );
     println!("{}", http_text(&list_url));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_candidate() {
+        let candidate_name = "java";
+        list_candidate(candidate_name);
+    }
 }
