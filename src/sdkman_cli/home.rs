c487916f0ee3cd05a284ac1d92c9d6bfe2de97d1
Libing Chen <libing.chen@gmail.com>
2024-09-18 18:04:45 +0800

    chore: polishing

--- src/sdkman_cli/home.rs
+++ src/sdkman_cli/home.rs
@@ -1,8 +1,8 @@
 use crate::sdkman_cli::{find_candidate_home};
 
-pub fn manage_home(default_matches: &clap::ArgMatches) {
-    let candidate_name = default_matches.get_one::<String>("candidate").unwrap();
-    let candidate_version = default_matches.get_one::<String>("version").unwrap();
+pub fn manage_home(home_matches: &clap::ArgMatches) {
+    let candidate_name = home_matches.get_one::<String>("candidate").unwrap();
+    let candidate_version = home_matches.get_one::<String>("version").unwrap();
     let candidate_home = find_candidate_home(candidate_name, candidate_version);
     if candidate_home.exists() {
         println!("{}", candidate_home.to_str().unwrap());
