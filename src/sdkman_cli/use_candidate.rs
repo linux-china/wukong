e80b0250cd0c3aa2e185b3e951fd0e8f0f7fcf47
Libing Chen <libing.chen@gmail.com>
2024-09-18 17:47:57 +0800

    app: use sub command for sdkman

---
+++ src/sdkman_cli/use_candidate.rs
@@ -0,0 +1,17 @@
+use crate::sdkman_cli::{find_candidate_home, sdkman_home};
+
+pub fn manage_use(use_matches: &clap::ArgMatches) {
+    let candidate_name = use_matches.get_one::<String>("candidate").unwrap();
+    let candidate_version = use_matches.get_one::<String>("version").unwrap();
+    let candidate_home = find_candidate_home(candidate_name, candidate_version);
+    if candidate_home.exists() {
+        let candidate_home_name = format!("{}_HOME", candidate_name.to_uppercase());
+        let home_path = candidate_home.to_str().unwrap();
+        println!("export PATH=\"{}:$PATH\"", home_path);
+        println!("export {}=\"{}\"", candidate_home_name, home_path);
+        println!("# Run this command to configure your shell:");
+        println!("# eval $(sdk use {} {})", candidate_name, candidate_version);
+    } else {
+        eprintln!("{}@{} not installed, please install it first!", candidate_name, candidate_version);
+    }
+}
