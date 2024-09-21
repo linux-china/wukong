38ac2faac110f5b13058f59520abc4b5bec05697
Libing Chen <libing.chen@gmail.com>
2024-09-21 22:20:43 +0800

    chore: add test

--- src/jbang_cli/models.rs
+++ src/jbang_cli/models.rs
@@ -21,6 +21,7 @@ struct CatalogRef {
     pub catalog_ref: String,
     pub description: Option<String>,
     #[serde(rename = "import")]
+    #[serde(default = "bool::default")]
     pub import_items: bool,
 }
 
@@ -36,4 +37,18 @@ struct TemplateProperty {
     pub description: String,
     #[serde(rename = "default")]
     pub default_value: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use crate::jbang_cli::jbang_home;
+    use super::*;
+
+    #[test]
+    fn test_read_default_jbang_catalog() {
+        let jbang_catalog_json = jbang_home().join("jbang-catalog.json");
+        let catalog: JBangCatalog = serde_json::from_reader(File::open(jbang_catalog_json).unwrap()).unwrap();
+        println!("{:?}", catalog);
+    }
 }
