1095a59197b989a1d4d1b417f8efe54379b7ee3e
Libing Chen <libing.chen@gmail.com>
2024-09-22 14:52:40 +0800

    app: cache sub command

---
+++ src/jbang_cli/cache.rs
@@ -0,0 +1,70 @@
+use clap::{Arg, Command};
+use itertools::Itertools;
+use crate::jbang_cli::call_jbang_sub_command;
+
+pub fn manage_cache(cache_matches: &clap::ArgMatches) {
+    if let Some((sub_command, matches)) = cache_matches.subcommand() {
+        match sub_command {
+            "clear" => {
+                let args = std::env::args().collect::<Vec<String>>();
+                let app_args = &args[3..].iter().map(|s| s.as_str()).collect_vec();
+                let mut command_args = vec!["cache", "clear"];
+                command_args.extend(app_args);
+                call_jbang_sub_command(&command_args);
+            }
+            _ => {}
+        }
+    }
+}
+
+
+pub fn build_cache_command() -> Command {
+    Command::new("cache")
+        .about("Manage compiled scripts in the local cache.")
+        .subcommand(
+            Command::new("clear")
+                .about("Clear the various caches used by jbang. By default this will clear the JAR, script, stdin and URL caches. To clear other caches list them explicitly i.e. '--project' for temporary projects.")
+                .arg(
+                    Arg::new("all")
+                        .help("clear all caches")
+                        .long("all")
+                        .num_args(0)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new("deps")
+                        .help("clear dependency cache only")
+                        .long("deps")
+                        .num_args(0)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new("groovyc")
+                        .help("clear groovyc cache only")
+                        .long("groovyc")
+                        .num_args(0)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new("kotlinc")
+                        .help("clear kotlinc cache only")
+                        .long("kotlinc")
+                        .num_args(0)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new("jar")
+                        .help("clear JAR cache only")
+                        .long("jar")
+                        .num_args(0)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new("jdk")
+                        .help("clear JDK cache only")
+                        .long("jdk")
+                        .num_args(0)
+                        .required(false)
+                )
+        )
+}
