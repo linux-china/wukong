358fa4302dd5dcb49dfd081fe656b6622cc5e4d6
Libing Chen <libing.chen@gmail.com>
2024-10-05 22:10:11 +0800

    chore: add auto candidate installl

--- src/sdkman_cli/use_candidate.rs
+++ src/sdkman_cli/use_candidate.rs
@@ -1,17 +1,25 @@
 use crate::sdkman_cli::{find_candidate_home};
+use crate::sdkman_cli::install::install_candidate;
 
 pub fn manage_use(use_matches: &clap::ArgMatches) {
     let candidate_name = use_matches.get_one::<String>("candidate").unwrap();
     let candidate_version = use_matches.get_one::<String>("version").unwrap();
     let candidate_home = find_candidate_home(candidate_name, candidate_version);
+    if !candidate_home.exists() {
+        install_candidate(candidate_name, candidate_version);
+    }
     if candidate_home.exists() {
         let candidate_home_name = format!("{}_HOME", candidate_name.to_uppercase());
         let home_path = candidate_home.to_str().unwrap();
-        println!("export PATH=\"{}:$PATH\"", home_path);
+        if candidate_home.join("bin").exists() {
+            println!("export PATH=\"{}:$PATH\"", home_path);
+        } else {
+            println!("export PATH=\"{}/bin:$PATH\"", home_path);
+        }
         println!("export {}=\"{}\"", candidate_home_name, home_path);
         println!("# Run this command to configure your shell:");
         println!("# eval $(sdk use {} {})", candidate_name, candidate_version);
     } else {
-        eprintln!("{}@{} not installed, please install it first!", candidate_name, candidate_version);
+        eprintln!("{}@{} not found, please check name and version correct or not!", candidate_name, candidate_version);
     }
 }
