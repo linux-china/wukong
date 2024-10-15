33bd0c2dfaa462849a17602d235e2b2d85bf34b4
Libing Chen <libing.chen@gmail.com>
2024-10-15 17:08:54 +0800

    feat: implement add, remove, list for templates

--- src/jbang_cli/template.rs
+++ src/jbang_cli/template.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
 use clap::{Arg, Command};
 use lazy_static::lazy_static;
-
+use url::Url;
+use crate::jbang_cli::jbang_catalog;
+use crate::jbang_cli::models::Template;
 
 lazy_static! {
     pub static ref TEMPLATES: HashMap<&'static str, &'static str> = {
@@ -19,7 +22,58 @@ pub fn manage_template(template_matches: &clap::ArgMatches) {
             "list" => {
                 list_templates();
             }
+            "add" => {
+                add_template(matches);
+            }
+            "remove" => {
+                remove_template(matches);
+            }
             _ => {}
+        }
+    }
+}
+
+pub fn add_template(matches: &clap::ArgMatches) {
+    let name = matches.get_one::<String>("name").unwrap();
+    let description = matches.get_one::<String>("description").map(|item| item.to_string());
+    let file = matches.get_one::<String>("file").unwrap();
+    let mut file_refs: HashMap<String, String> = HashMap::new();
+    if file.starts_with("http://") || file.starts_with("https://") {
+        let url = Url::parse(file).unwrap();
+        let url_path = url.path();
+        if let Some(pos) = url_path.rfind('/') {
+            let file_name = &url_path[pos + 1..];
+            let ext_name = if let Some(pos) = file_name.rfind('.') {
+                file_name[pos + 1..].to_string()
+            } else {
+                "java".to_owned()
+            };
+            file_refs.insert(format!("{{basename}}.{}", ext_name), file.clone());
+        }
+    } else {
+        let file_path = PathBuf::from(file);
+        let absolute_path = std::fs::canonicalize(&file_path).unwrap();
+        let absolute_path = absolute_path.as_path();
+        let ext_name = absolute_path.extension().unwrap().to_str().unwrap();
+        file_refs.insert(format!("{{basename}}.{}", ext_name), absolute_path.to_str().unwrap().to_string());
+    }
+    let template = Template {
+        file_refs: file_refs,
+        description: description,
+        properties: None,
+    };
+    let mut jbang_catalog = jbang_catalog();
+    jbang_catalog.add_template(name, template);
+    jbang_catalog.write_default();
+}
+
+pub fn remove_template(matches: &clap::ArgMatches) {
+    let name = matches.get_one::<String>("name").unwrap();
+    let mut jbang_catalog = jbang_catalog();
+    if let Some(templates) = &jbang_catalog.templates {
+        if templates.contains_key(name) {
+            jbang_catalog.remove_template(name);
+            jbang_catalog.write_default();
         }
     }
 }
@@ -28,6 +82,13 @@ pub fn list_templates() {
     for (key, value) in TEMPLATES.iter() {
         println!("{}", *key);
         println!("  {}", *value);
+    }
+    let jbang_catalog = jbang_catalog();
+    if let Some(templates) = &jbang_catalog.templates {
+        for (key, value) in templates.iter() {
+            println!("{}", key);
+            println!("  {}", value.description.as_ref().unwrap_or(&"No description".to_string()));
+        }
     }
 }
 pub fn build_template_command() -> Command {
