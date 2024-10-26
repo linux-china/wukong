dce214aebad46d0201bc3ed3efab3b63bfbf6285
Libing Chen <libing.chen@gmail.com>
2024-10-26 22:00:37 +0800

    chore: add GRAALVM_HOME and java major version support

--- src/sdkman_cli/use_candidate.rs
+++ src/sdkman_cli/use_candidate.rs
@@ -1,12 +1,28 @@
-use crate::sdkman_cli::{find_candidate_home};
+use crate::sdkman_cli::{find_candidate_home, find_java_home, find_java_version};
 use crate::sdkman_cli::install::install_candidate;
 
 pub fn manage_use(use_matches: &clap::ArgMatches) {
     let candidate_name = use_matches.get_one::<String>("candidate").unwrap();
-    let candidate_version = use_matches.get_one::<String>("version").unwrap();
-    let candidate_home = find_candidate_home(candidate_name, candidate_version);
+    let mut candidate_version = use_matches.get_one::<String>("version").unwrap().to_string();
+    // find java home by major version
+    if candidate_name == "java" && candidate_version.parse::<u32>().is_ok() {
+        if let Some(java_home) = find_java_home(&candidate_version) {
+            println!("export JAVA_HOME=\"{}\"", java_home.to_str().unwrap());
+            if java_home.as_os_str().to_str().unwrap().contains("graal") {
+                println!("export GRAALVM_HOME=\"{}\"", java_home.to_str().unwrap());
+            }
+            println!("export PATH=\"{}:$PATH\"", java_home.join("bin").to_str().unwrap());
+            println!("# Run this command to configure your shell:");
+            println!("# eval $(sdk use {} {})", candidate_name, candidate_version);
+            return;
+        } else {
+            let java_version = find_java_version(&candidate_version).unwrap();
+            candidate_version = java_version;
+        }
+    }
+    let candidate_home = find_candidate_home(candidate_name, &candidate_version);
     if !candidate_home.exists() {
-        install_candidate(candidate_name, candidate_version);
+        install_candidate(candidate_name, &candidate_version);
     }
     if candidate_home.exists() {
         let candidate_home_name = format!("{}_HOME", candidate_name.to_uppercase());
@@ -17,6 +33,9 @@ pub fn manage_use(use_matches: &clap::ArgMatches) {
             println!("export PATH=\"{}/bin:$PATH\"", home_path);
         }
         println!("export {}=\"{}\"", candidate_home_name, home_path);
+        if candidate_name == "java" && candidate_version.contains("graal") {
+            println!("export GRAALVM_HOME=\"{}\"", home_path);
+        }
         println!("# Run this command to configure your shell:");
         println!("# eval $(sdk use {} {})", candidate_name, candidate_version);
     } else {
