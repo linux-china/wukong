a464d1d037ef327ea052be95813c3c5ddb181e87
Libing Chen <libing.chen@gmail.com>
2025-04-30 22:29:14 +0800

    test: add tests for read_url

--- tests/oneio_test.rs
+++ tests/oneio_test.rs
@@ -14,6 +14,26 @@ fn test_url_read() {
     dump(&url).unwrap();
 }
 
+#[test]
+fn test_read_url() {
+    let context = UrlContext::new();
+    let url = context.url("https://httpbin.org/ip").unwrap();
+    dump(&url).unwrap();
+}
+#[test]
+fn test_read_git() {
+    let context = UrlContext::new();
+    let url = context.url("git:https://github.com/linux-china/task-keeper.git!/justfile").unwrap();
+    dump(&url).unwrap();
+}
+
+#[test]
+fn test_read_jar() {
+    let context = UrlContext::new();
+    let url = context.url("zip:https://repo1.maven.org/maven2/commons-io/commons-io/2.18.0/commons-io-2.18.0.jar!/META-INF/maven/commons-io/commons-io/pom.xml").unwrap();
+    dump(&url).unwrap();
+}
+
 pub fn dump(url: &UrlRef) -> Result<(), UrlError> {
     let mut reader = url.open()?; // io::Read
     let mut string = String::new();
