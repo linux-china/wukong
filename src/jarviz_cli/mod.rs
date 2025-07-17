822fe9559996acd974c2ba5342d3ddfedb2eaa39
Libing Chen <libing.chen@gmail.com>
2025-07-17 14:44:43 -0700

    chore: polishing

--- src/jarviz_cli/mod.rs
+++ src/jarviz_cli/mod.rs
@@ -1,10 +1,10 @@
+use crate::jarviz_cli::bytecode::get_major_version;
 use colored::Colorize;
 use itertools::Itertools;
 use pad::PadStr;
-use prettytable::{row, Table};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::Read;
@@ -12,86 +12,8 @@ use std::path::Path;
 use walkdir::WalkDir;
 use zip::ZipArchive;
 
+pub mod bytecode;
 pub mod clap_app;
-
-fn get_major_version(zip_file: &mut dyn Read) -> u16 {
-    let mut buffer = [0; 8];
-    zip_file.read(&mut buffer).unwrap();
-    // check if the file is a class file
-    if &buffer[0..4] == b"\xCA\xFE\xBA\xBE" {
-        // get major version from buffer[6] and buffer[7]
-        let major_version = ((buffer[6] as u16) << 8) | (buffer[7] as u16);
-        return major_version;
-    }
-    0
-}
-
-fn get_java_version(major_version: u16) -> &'static str {
-    match major_version {
-        45 => "1.1",
-        46 => "1.2",
-        47 => "1.3",
-        48 => "1.4",
-        49 => "5.0",
-        50 => "6",
-        51 => "7",
-        52 => "8",
-        53 => "9",
-        54 => "10",
-        55 => "11",
-        56 => "12",
-        57 => "13",
-        58 => "14",
-        59 => "15",
-        60 => "16",
-        61 => "17",
-        62 => "18",
-        63 => "19",
-        64 => "20",
-        65 => "21",
-        66 => "22",
-        67 => "23",
-        68 => "24",
-        69 => "25",
-        _ => "Unknown",
-    }
-}
-
-fn get_bytecode_version(java_version: f32) -> u16 {
-    match java_version {
-        1.1 => 45,
-        1.2 => 46,
-        1.3 => 47,
-        1.4 => 48,
-        1.5 => 49,
-        5.0 => 49,
-        6.0 => 50,
-        1.6 => 50,
-        7.0 => 51,
-        1.7 => 51,
-        8.0 => 52,
-        1.8 => 52,
-        9.0 => 53,
-        10.0 => 54,
-        11.0 => 55,
-        12.0 => 56,
-        13.0 => 57,
-        14.0 => 58,
-        15.0 => 59,
-        16.0 => 60,
-        17.0 => 61,
-        18.0 => 62,
-        19.0 => 63,
-        20.0 => 64,
-        21.0 => 65,
-        22.0 => 66,
-        23.0 => 67,
-        24.0 => 68,
-        25.0 => 69,
-        26.0 => 70,
-        _ => (java_version - 44.0) as u16,
-    }
-}
 
 fn scan_local_archive<P: AsRef<Path>>(
     path: P,
@@ -191,15 +113,6 @@ fn archive_manifest_local<P: AsRef<Path>>(path: P) -> Option<String> {
         })
 }
 
-pub fn bytecode(command_matches: &clap::ArgMatches) {
-    if let Some((command, command_matches)) = command_matches.subcommand() {
-        match command {
-            "show" => bytecode_show(command_matches),
-            &_ => println!("Unknown command"),
-        }
-    }
-}
-
 fn get_local_jar(group_id: &str, artifact_id: &str, version: &str) -> Option<String> {
     let m2_home = dirs::home_dir().unwrap().join(".m2");
     let gradle_home = dirs::home_dir().unwrap().join(".gradle");
@@ -238,7 +151,7 @@ fn get_local_jar(group_id: &str, artifact_id: &str, version: &str) -> Option<Str
     None
 }
 
-fn resolve_jar_source(command_matches: &clap::ArgMatches) -> Option<String> {
+fn resolve_jar_endpoint(command_matches: &clap::ArgMatches) -> Option<String> {
     if let Some(gav) = command_matches.get_one::<String>("gav") {
         let parts = gav.split(":").collect::<Vec<&str>>();
         let group_id = parts[0];
@@ -260,170 +173,12 @@ fn resolve_jar_source(command_matches: &clap::ArgMatches) -> Option<String> {
         return Some(url.clone());
     } else if let Some(directory) = command_matches.get_one::<String>("directory") {
         return Some(format!("dir://{}", directory));
+    } else if command_matches.get_flag("pom") {
+        return Some("pom://".to_owned());
+    } else if command_matches.get_flag("gradle") {
+        return Some("gradle://".to_owned());
     }
     None
-}
-
-fn resolve_pom_dependencies(output: &str) -> HashSet<String> {
-    let start_placeholder = "--- dependency:";
-    let end_placeholder = "------";
-    let mut dependencies: HashSet<String> = HashSet::new();
-    let mut in_dependencies_section = false;
-    for line in output.lines() {
-        if line.contains(start_placeholder) && line.contains("tree") {
-            in_dependencies_section = true;
-            continue;
-        }
-        if line.contains(end_placeholder) {
-            in_dependencies_section = false;
-            continue;
-        }
-        if in_dependencies_section {
-            let mut trimmed_line = line.trim();
-            if trimmed_line.ends_with("(optional)") {
-                trimmed_line = &trimmed_line[..trimmed_line.len() - 10].trim();
-            }
-            if let Some(pos) = trimmed_line.rfind(" ") {
-                trimmed_line = &trimmed_line[pos + 1..];
-            }
-            if trimmed_line.contains(":") {
-                dependencies.insert(trimmed_line.to_string());
-            }
-        }
-    }
-    dependencies
-}
-
-fn resolve_gradle_dependencies(output: &str) -> HashSet<String> {
-    let start_placeholder = "compileClasspath";
-    let mut dependencies: HashSet<String> = HashSet::new();
-    let mut in_dependencies_section = false;
-    for line in output.lines() {
-        if line.starts_with(start_placeholder) || line.starts_with("runtimeClasspath") {
-            in_dependencies_section = true;
-            continue;
-        }
-        if line.trim().is_empty() {
-            in_dependencies_section = false;
-            continue;
-        }
-        if in_dependencies_section {
-            let mut trimmed_line = line.trim();
-            if trimmed_line.ends_with(")") {
-                let offset = trimmed_line.rfind("(").unwrap();
-                trimmed_line = &trimmed_line[..offset].trim();
-            }
-            if let Some(pos) = trimmed_line.rfind(" ") {
-                trimmed_line = &trimmed_line[pos + 1..];
-            }
-            if trimmed_line.contains(":") {
-                dependencies.insert(trimmed_line.to_string());
-            }
-        }
-    }
-    dependencies
-}
-
-pub fn bytecode_show(command_matches: &clap::ArgMatches) {
-    let mut class_info = HashMap::<u16, u32>::new();
-    let include_details: bool = command_matches.get_flag("details");
-    let mut class_details = HashMap::<u16, Vec<String>>::new();
-    let mut default_bytecode_version = command_matches
-        .get_one::<String>("java-version")
-        .map(|s| s.parse::<u16>().unwrap_or(0))
-        .unwrap_or(0);
-    if let Some(java_version) = command_matches.get_one::<String>("java-version") {
-        if let Ok(version) = java_version.parse::<f32>() {
-            default_bytecode_version = get_bytecode_version(version);
-        }
-    }
-    if let Some(jar_source) = resolve_jar_source(command_matches) {
-        if jar_source.starts_with("file://") {
-            let local_path = jar_source.trim_start_matches("file://");
-            scan_local_archive(
-                local_path,
-                &mut class_info,
-                include_details,
-                &mut class_details,
-            );
-        } else if jar_source.starts_with("dir://") {
-            let directory = jar_source.trim_start_matches("dir://");
-            for entry in WalkDir::new(directory).into_iter().filter_map(|e| e.ok()) {
-                if entry.file_type().is_file() {
-                    let path = entry.path();
-                    if path.to_str().unwrap().ends_with(".jar") {
-                        scan_local_archive(
-                            path,
-                            &mut class_info,
-                            include_details,
-                            &mut class_details,
-                        );
-                    }
-                }
-            }
-        } else {
-            scan_remote_archive(
-                &jar_source,
-                &mut class_info,
-                include_details,
-                &mut class_details,
-            );
-        }
-    }
-    if class_info.is_empty() {
-        println!("No class files found in the provided input.");
-        return;
-    }
-    let mut table = Table::new();
-    table.add_row(row!["Major Version", "Java Version", "Class Count"]);
-    // iterate through the class_info map and print the major version and count
-    for (major_version, count) in class_info.iter().sorted_by_key(|x| x.1) {
-        let java_version = get_java_version(*major_version);
-        table.add_row(row![
-            major_version.to_string(),
-            java_version,
-            count.to_string()
-        ]);
-    }
-    let default_format = "text".to_owned();
-    let output_format = command_matches
-        .get_one::<String>("output-format")
-        .unwrap_or(&default_format);
-    if output_format == "csv" {
-        table.to_csv(io::stdout()).unwrap();
-    } else {
-        table.printstd();
-        if include_details {
-            if default_bytecode_version > 0 {
-                println!(
-                    "\nClasses with major version {} (Java {}):",
-                    default_bytecode_version,
-                    get_java_version(default_bytecode_version)
-                );
-                if let Some(classes) = class_details.get(&default_bytecode_version) {
-                    for class_full_name in classes {
-                        println!("  - {}", class_full_name);
-                    }
-                } else {
-                    println!(
-                        "  No classes found for major version:{}",
-                        default_bytecode_version
-                    );
-                }
-            } else {
-                for (major_version, classes) in class_details.iter().sorted_by_key(|x| x.1) {
-                    println!(
-                        "### {}(Java {})",
-                        major_version,
-                        get_java_version(*major_version)
-                    );
-                    for class_full_name in classes {
-                        println!("  - {}", class_full_name);
-                    }
-                }
-            }
-        }
-    }
 }
 
 pub fn entries(command_matches: &clap::ArgMatches) {
@@ -467,7 +222,7 @@ pub fn jar_manifest(command_matches: &clap::ArgMatches) {
 }
 
 pub fn jar_manifest_show(command_matches: &clap::ArgMatches) {
-    if let Some(jar_source) = resolve_jar_source(command_matches) {
+    if let Some(jar_source) = resolve_jar_endpoint(command_matches) {
         let manifest = if jar_source.starts_with("file://") {
             let local_path = jar_source.trim_start_matches("file://");
             archive_manifest_local(local_path)
@@ -485,7 +240,7 @@ pub fn jar_manifest_show(command_matches: &clap::ArgMatches) {
 }
 
 pub fn jar_manifest_query(command_matches: &clap::ArgMatches) {
-    if let Some(jar_source) = resolve_jar_source(command_matches) {
+    if let Some(jar_source) = resolve_jar_endpoint(command_matches) {
         let manifest = if jar_source.starts_with("file://") {
             let local_path = jar_source.trim_start_matches("file://");
             archive_manifest_local(local_path)
@@ -566,14 +321,6 @@ mod tests {
     use std::path::PathBuf;
 
     #[test]
-    fn test_bytecode() {
-        let mcs_app = build_jarviz_app();
-        let mcs_matches = mcs_app.get_matches_from(&vec!["mcs", "class-search", "VelocityEngine"]);
-        let class_search_matches = mcs_matches.subcommand_matches("class-search").unwrap();
-        bytecode(class_search_matches);
-    }
-
-    #[test]
     fn test_jar_extract() {
         let path = home_dir()
             .unwrap()
@@ -622,7 +369,6 @@ mod tests {
         let mcs_matches =
             mcs_app.get_matches_from(&vec!["mcs", "search", "spring-messaging", "--format=maven"]);
         let class_search_matches = mcs_matches.subcommand_matches("search").unwrap();
-        bytecode(class_search_matches);
     }
 
     #[test]
@@ -665,25 +411,7 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_resolve_pom_dependencies() {
-        let path = PathBuf::from("tests/dependencies/maven-dependencies.txt");
-        let output = std::fs::read_to_string(path).unwrap();
-        let dependencies = resolve_pom_dependencies(&output);
-        for dependency in dependencies {
-            println!("{}", dependency);
-        }
-    }
 
-    #[test]
-    fn test_resolve_gradle_dependencies() {
-        let path = PathBuf::from("tests/dependencies/gradle-dependencies.txt");
-        let output = std::fs::read_to_string(path).unwrap();
-        let dependencies = resolve_gradle_dependencies(&output);
-        for dependency in dependencies {
-            println!("{}", dependency);
-        }
-    }
 
     #[test]
     fn test_find_local_jar() {
