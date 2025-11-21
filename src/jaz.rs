7a0f86759be023ceb4a93270d5bd60c40ce98b49
Liging Chen <libing.chen@gmail.com>
2025-11-21 14:29:31 +0800

    chore: add java home and java version

--- src/jaz.rs
+++ src/jaz.rs
@@ -1,6 +1,13 @@
 use dotenvx_rs::dotenvx;
 use std::env;
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
+
+struct JavaInfo {
+    home: String,
+    version: String,
+    vendor: String,
+}
 
 fn main() {
     let jaz_args = env::args().collect::<Vec<_>>();
@@ -32,4 +39,75 @@ fn main() {
         1
     };
     std::process::exit(exit_code);
+}
+
+fn get_java_info() -> JavaInfo {
+    let java_home = get_java_home().unwrap();
+    let java_home_path = PathBuf::from(&java_home);
+    let java_exe_path = java_home_path.join("bin").join("java");
+    let mut command = Command::new(java_exe_path);
+    command.arg("--version");
+    let output = command.output().expect("Failed to execute command");
+    let version_output = String::from_utf8_lossy(&output.stdout);
+    let version_lines: Vec<&str> = version_output.lines().collect();
+    let version_line = version_lines[0];
+    let parts: Vec<&str> = version_line
+        .split(" ")
+        .map(|s| s.trim_matches(|c| c == '"' || c == '\''))
+        .collect();
+    let mut java_version = parts.iter().find(|part| part[0..2].parse::<i32>().is_ok());
+
+    JavaInfo {
+        home: java_home,
+        version: java_version.unwrap_or(&"21").to_string(),
+        vendor: "".to_string(),
+    }
+}
+
+pub fn get_java_home() -> Option<String> {
+    if let Ok(java_home) = env::var("JAVA_HOME") {
+        return Some(java_home);
+    } else if let Ok(java_exec) = which::which("java") {
+        if java_exec.is_symlink() {
+            if let Ok(target) = std::fs::read_link(&java_exec) {
+                return Some(
+                    target
+                        .parent()
+                        .unwrap()
+                        .parent()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string(),
+                );
+            }
+        } else {
+            return Some(
+                java_exec
+                    .parent()
+                    .unwrap()
+                    .parent()
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string(),
+            );
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_java_version() {
+        let info = get_java_info();
+        println!("Java version: {}", info.version)
+    }
+
+    #[test]
+    fn test_get_java_home() {
+        let java_home = get_java_home();
+        println!("{:?}", java_home);
+    }
 }
